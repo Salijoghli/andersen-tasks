119b587e3c6d1502706457bbb2059a86ff5696dd
Salijoghli <guga.kakhadze12@gmail.com>
2025-03-13 18:19:44 +0400

    tasks-5: Queue class

--- tasks-5/script.js
+++ tasks-5/script.js
@@ -30,7 +30,7 @@ class Stack extends BaseStorage {
   }
   pop() {
     if (this.isEmpty()) throw new Error("Stack is Empty. Cannot pop.");
-    this._getStorage().pop();
+    return this._getStorage().pop();
   }
   peek() {
     const storage = this._getStorage();
@@ -40,10 +40,38 @@ class Stack extends BaseStorage {
   static fromIterable(iterable) {
     if (iterable == null || typeof iterable[Symbol.iterator] !== "function")
       throw new Error(`${iterable} isn't iterable`);
-    const stack = new Stack();
-    for (const element of iterable) {
+    const tempArray = Array.from(iterable);
+    const stack = new Stack(tempArray.length);
+    for (const element of tempArray) {
       stack.push(element);
     }
     return stack;
+  }
+}
+
+class Queue extends BaseStorage {
+  push(elem) {
+    const storage = this._getStorage();
+    if (storage.length == this.maxSize)
+      throw new Error("Cannot push, queue is full.");
+    storage.push(elem);
+  }
+  shift() {
+    if (this.isEmpty()) throw new Error("Stack is Empty. Cannot pop.");
+    return this._getStorage().shift();
+  }
+  peek() {
+    const storage = this._getStorage();
+    return storage[0] ?? null;
+  }
+  static fromIterable(iterable) {
+    if (iterable == null || typeof iterable[Symbol.iterator] !== "function")
+      throw new Error(`${iterable} isn't iterable`);
+    const tempArray = Array.from(iterable);
+    const queue = new Queue(tempArray.length);
+    for (const element of tempArray) {
+      queue.push(element);
+    }
+    return queue;
   }
 }
